9e2fb78d682b3409cfc99e127c69786d47107ec3
Aculeos <ehba.andrade@gmail.com>
2023-11-10 23:39:29 -0300

    alterações feitas

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,32 +1,32 @@
 
-function valida(){
-    if(window.confirm("Você prefere backend?")==true){
-    window.location.href="node-js.html"
+function valida() {
+    if (window.confirm("Você prefere backend?") == true) {
+        window.location.href = "node-js.html"
     }
 }
-function voltar(){
-    if(volta.checked){
-        window.location.href="Index.html"
-    }else{
+function voltar() {
+    if (volta.checked) {
+        window.location.href = "Index.html"
+    } else {
         alert("clique no checkbox")
     }
-    }
-function TrocarIMG(){
-    
+}
+function TrocarIMG() {
+
     var opcao = document.getElementById("Passatempo");
     var imagemTroca = document.getElementById("ImagemGM");
 
     if (opcao.value == 1) {
         imagemTroca.src = "../Imagens/Minecraft.jpeg";
     } else if (opcao.value == 2) {
-    imagemTroca.src = "../Imagens/Age-Of-Mythology.jpeg"
+        imagemTroca.src = "../Imagens/Age-Of-Mythology.jpeg"
     } else if (opcao.value == 3) {
-    imagemTroca.src = "../Imagens/Age-Of-Empires-III.jpeg"
+        imagemTroca.src = "../Imagens/Age-Of-Empires-III.jpeg"
     } else if (opcao.value == 4) {
-    imagemTroca.src = "../Imagens/Terraria.jpeg"
+        imagemTroca.src = "../Imagens/Terraria.jpeg"
     } else if (opcao.value == 5) {
-    imagemTroca.src = "../Imagens/StardewValley.jpeg"
+        imagemTroca.src = "../Imagens/StardewValley.jpeg"
     } else if (opcao.value == 6) {
-    imagemTroca.src = "../Imagens/Factorio.jpeg"
+        imagemTroca.src = "../Imagens/Factorio.jpeg"
     }
 }
